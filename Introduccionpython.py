9c183692cd5836f20a1e43c0366d4cfb97a2d58d
inaki1111 <inakirm111@gmail.com>
2023-01-31 19:23:54 -0600

    primer commit

---
+++ Introduccionpython.py
@@ -0,0 +1,42 @@
+import math
+
+# tipo de variables 
+# int numero entero positivo 
+# float numero decimal positivo
+# str cadena de caracteres  
+# bool verdadero o falso
+# list lista de elementos
+
+# asignacion de variables
+
+#nombre = "Juan"   # doble entre comillas cuando hablamos de una cadena de caracteres "palabra"
+#numero = 5       # numero entero positivo
+#decimal = 5.5    # numero decimal positivo
+
+# imprimir en pantalla
+
+#print(5) # imprime el numero 5
+
+
+nombre = input("Ingrese su nombre: ") # imprime en pantalla la cadena de caracteres "Ingrese su nombre: " y espera que el usuario ingrese un valor y lo guarda en la variable nombre
+print(nombre) # imprime el valor de la variable nombre
+
+
+# operadores aritmeticos
+# + suma
+# - resta
+# * multiplicacion
+# / division   shift + 7
+# ** potencia 
+# % modulo
+
+
+# Calculadora IMC
+
+#peso (kg) float
+#altura (m) float 
+#imc
+
+#imc = peso / altura ** 2
+
+#print(math.trunc(imc))
