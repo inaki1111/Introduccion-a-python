b70a9629b60ed43ccf23485084c36c741c36973f
inaki1111 <inakirm111@gmail.com>
2023-02-14 21:23:00 -0600

    archivos

---
+++ files.py
@@ -0,0 +1,24 @@
+#archivo = open("archivo.txt" , "r")
+#print(archivo.read(10))
+
+#print(archivo.readline())
+
+
+#archivo.close()
+
+# a means append
+
+#archivo = open("archivo.txt" , "a")
+
+#var = input("Ingrese un texto: ")
+#archivo.write(var)
+#archivo.close()
+
+
+
+archivo = open("archivo.txt" , "w")
+archivo.write("Borré la info anterior")
+archivo.close()
+
+
+
