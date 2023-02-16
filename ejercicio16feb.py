6c04a7427faa8634e96704af64d7ff741be125e4
inaki1111 <inakirm111@gmail.com>
2023-02-16 17:28:30 -0600

    Ejercicio 16 de feb

---
+++ ejercicio16feb.py
@@ -0,0 +1,11 @@
+#Crear un programa en tkinter donde se muestre la información de un archivo de texto Que se encuentra en la carpeta del programa
+#El archivo de texto debe tener el siguiente formato:
+#Nombre: Juan Perez
+#Edad: 25
+#Carrera: Ingeniería en Sistemas
+#Universidad: Universidad Nacional de Asunción
+#El programa debe mostrar la información en un label y un entry para cada uno de los datos del archivo de texto
+
+#El programa debe tener un botón para salir del programa
+
+
